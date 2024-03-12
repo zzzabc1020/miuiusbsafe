f8aed69f4e3376e70ae9cdbff843eb17d5e72472
zzzabc1020 <157718556+zzzabc1020@users.noreply.github.com>
2024-03-12 14:42:00 +0800

    Update usb.sh

--- usb.sh
+++ usb.sh
@@ -1,9 +1,1 @@
-sleep
-setprop persist.security.adbinstall 1
-setprop persist.security.adbinput 1
-setprop persist.fastboot.enable 1
-sleep 3
-am force-stop com.miui.securitycenter
-sleep 2
-sed -i '267i\<boolean name="permcenter_install_intercept_enabled" value="false" />\n<boolean name="security_adb_install_enable" value="true" />' /data/data/com.miui.securitycenter/shared_prefs/emote_provider_preferences.xml
-sed -i 's/<boolean name="perm_adb_install_notify" value="true" \/>/<boolean name="perm_adb_install_notify" value="false" \/>/' /data/data/com.miui.securitycenter/shared_prefs/remote_provider_preferences.xml
+echon 123212
